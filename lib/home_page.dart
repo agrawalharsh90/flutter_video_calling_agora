bc9bf784277ce297abfd9aa13de8a311bc7aebf7
Harsh Agrawal <agrawalharsh90@gmail.com>
2020-06-21 06:41:05 +0530

    video calling/Streaming added

---
+++ lib/home_page.dart
@@ -0,0 +1,131 @@
+import 'dart:async';
+
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_video_calling_agora/call_page.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  final _channelController = TextEditingController();
+  bool _validateError = false;
+
+  ClientRole _role = ClientRole.Broadcaster;
+
+  @override
+  void dispose() {
+    _channelController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Harsh Agrawal'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: 400,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                    controller: _channelController,
+                    decoration: InputDecoration(
+                      errorText:
+                          _validateError ? 'Channel name is mandatory' : null,
+                      border: UnderlineInputBorder(
+                        borderSide: BorderSide(width: 1),
+                      ),
+                      hintText: 'Channel name',
+                    ),
+                  ))
+                ],
+              ),
+              Column(
+                children: [
+                  ListTile(
+                    title: Text("Join as Broadcaster"),
+                    leading: Radio(
+                      value: ClientRole.Broadcaster,
+                      groupValue: _role,
+                      onChanged: (ClientRole value) {
+                        setState(() {
+                          _role = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text("Join as Audience"),
+                    leading: Radio(
+                      value: ClientRole.Audience,
+                      groupValue: _role,
+                      onChanged: (ClientRole value) {
+                        setState(() {
+                          _role = value;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        onPressed: onJoin,
+                        child: Text('Join'),
+                        color: Colors.redAccent,
+                        textColor: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> onJoin() async {
+    // update input validation
+    setState(() {
+      _channelController.text.isEmpty
+          ? _validateError = true
+          : _validateError = false;
+    });
+    if (_channelController.text.isNotEmpty) {
+      // await for camera and mic permissions before pushing video page
+      await _handleCameraAndMic();
+      // push video page with given channel name
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CallPage(
+            channelName: _channelController.text,
+            role: _role,
+          ),
+        ),
+      );
+    }
+  }
+
+  Future<void> _handleCameraAndMic() async {
+    await PermissionHandler().requestPermissions(
+      [PermissionGroup.camera, PermissionGroup.microphone],
+    );
+  }
+}
